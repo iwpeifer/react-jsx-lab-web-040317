8dd648fea141b950aaea09dd9cd91145b65bd6ea
iwpeifer <iwpeifer@gmail.com>
2017-06-05 16:32:45 -0400

    Done.

---
+++ src/components/Webpage.js
@@ -0,0 +1,18 @@
+import FillerText from './FillerText'
+import RegistrationForm from './RegistrationForm'
+
+import React from 'react';
+
+class Webpage extends React.Component {
+  render() {
+    return (
+      <div>
+        <title>The world's coolest webpage</title>
+        <FillerText/>
+        <FillerText/>
+      </div>
+    );
+  }
+}
+
+export default Webpage;
